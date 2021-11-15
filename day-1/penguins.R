6e037efeea10706097964039bc79f4e1ae0ab3ad
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-11-15 15:21:05 +0800

    notes from meeting with Nick G

--- day-1/penguins.R
+++ day-1/penguins.R
@@ -97,10 +97,6 @@ draws <- mcmc(m)
 # visualise the MCMC traces
 plot(draws)
 
-# we can also use bayesplot to explore the convergence of the model
-mcmc_trace(draws)
-mcmc_dens(draws)
-
 # check convergence (we already discarded burn-in and don't need the
 # multivariate stat)
 coda::gelman.diag(draws, autoburnin = FALSE, multivariate = FALSE)
@@ -108,103 +104,4 @@ coda::gelman.diag(draws, autoburnin = FALSE, multivariate = FALSE)
 # look at the parameter estimates
 summary(draws)
 
-## Posterior predictive check
-
-# we want to see how well the data match the predictions from the model that
-# we have fit
-# to do this we are going to do a graphical "posterior predictive check" (or
-# PPC for short).
-# you can think of this as being analogous to comparing your data to the model
-# predictions. If the model and the data are similar, we've done a good job
-# fitting our model. If they are not similar, our model doesn't represent the
-# data very well
-# There are some helpful ways to visualise this build into the "bayesplot" R
-# package. But we need to do some summaries of the data first.
-# this next step is inspired by the well worked vignette,
-# "graphical PPCs", found at:
-# https://cran.r-project.org/web/packages/bayesplot/vignettes/graphical-ppcs.html
-library(bayesplot)
-
-# to do this we need some simulated predictions
-# in a regular glm model, you might do something like:
-# predict(glm_model)
-# which would produce a vector of model predictions the same length as the
-# data
-# we will use greta's `calculate` function, which will act in a similar way
-# but has a lot of other uses and is very flexible
-# for the moment, we will focus on this specific use, for calculating predictions
-# We take our vector, y, which is out outcome
-# then we tell it to use the draws object
-# and to calculate 500 simulations
-yrep_model <- calculate(
-  y,
-  values = draws,
-  nsim = 50
-)
-
-# each row represents a draw from the posterior predictive distribution
-# There is one element for each of the datapoints in Y
-# there were 333 rows in the data:
-length(y)
-# and then there are 500 rows, one for each simulation we drew earlier.
-# given that there are
-dim(yrep_model$y)
-str(y)
-# What we require here is a matrix
-# where the rows are the number of draws
-# and the columns are the number of observations
-# we convert this with a special helper function, ppc_matrix
-ppc_matrix <- function(x){
-  matrix(data = x,
-         nrow = dim(x)[1],
-         ncol = dim(x)[2],
-         byrow = TRUE)
-}
-
-yrep_matrix <- ppc_matrix(yrep_model$y)
-y_values <- as.integer(y)
-
-ppc_dens_overlay(y = y_values,
-                 yrep = yrep_matrix)
-
-# we see that there is wide variation in model estimates and data
-
-# Another way to look at this is by looking at individual histograms
-# of y and yrep data with ppc_hist
-# here we control the number of plots by subsetting the matrix to be the first
-# 5
-ppc_hist(y = y_values,
-         yrep = yrep_matrix[1:5,])
-
-## distribution of test statistics
-
-# we can  look at the distribution of ones over the replicated datasets
-# from the posterior predictive distribution in yrep_matrix and compare to the
-# proportion of observed ones in y.
-
-# we define a function that tells us the proportion of ones
-prop_ones <- function(x) mean(x == 1)
-prop_ones(y_values) # check proportion of ones in y
-
-# We can visualise the proportion of ones in the simulations from the model
-ppc_stat(y_values,
-         yrep_matrix,
-         stat = "prop_ones",
-         binwidth = 0.005)
-
-# we see there is variability in the estimate compared to the data
-
-# some other ppc variants
-
-ppc_ecdf_overlay(
-  y_values,
-  yrep_matrix
-)
-
-ppc_dens_overlay_grouped(y_values,
-                         yrep_matrix,
-                         group = penguins_for_modelling$island)
-
-ppc_ecdf_overlay_grouped(y_values,
-                         yrep_matrix,
-                         group = penguins_for_modelling$island)
+# Instructors proceed with live coding
